87185fb3cc081f98638f9a8dbebc5f22b9c91439
rustxd2171 <nathaniemzinahbizu@gmail.com>
2024-11-04 20:11:32 +0300

    Add files via upload

---
+++ hamburger.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active'); // Toggles the active class to show/hide the menu
+});
